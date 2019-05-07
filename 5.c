0f6fd02d97fe6b5428b67c5f0e5ff39e8f6640e7
Matchbox1233 <43754339+Matchbox1233@users.noreply.github.com>
2019-05-07 06:48:32 +0200

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-    int main()
-     {
-        printf("A szóhossz ezen a gépen: %d bites\n", sizeof(int)*8);
-        return 0;
-     }
+int main()
+{
+	int n=1;
+	int x=1;
+	while(x<<=1) n++;
+	printf("A gépi szó hossza: %d\n",n);
+	return 0;
+}
